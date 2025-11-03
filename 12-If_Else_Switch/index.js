3466d0ce0d84a3ae51893cfe06b9c8eaec5dee37
Gtavogubs <gustavo.benedito.justino@gmail.com>
2025-11-03 11:44:29 -0300

    jogodamemória

--- 12-If_Else_Switch/index.js
+++ 12-If_Else_Switch/index.js
@@ -1,44 +1,44 @@
-function ligar(){
-    var valor = document.getElementById("numero").value;
+function ligar() {
+    var valor1 = document.getElementById("numero").value;
     var valor2 = 14;
-    
-    if(valor1 == valor2) {
+
+    if (valor1 == valor2) {
         document.getElementById("botao1").style.backgroundColor = "yellow";
         document.getElementById("botao1").style.color = "red";
         document.getElementById("numero2").innerHTML = "Acertou!";
     } else {
-        if (valor1 < 10){
+        if (valor1 < 10) {
             document.getElementById("numero2").innerHTML = "Não foi dessa vez!";
         }
-        if (valor1 < 9 && valor1 <14){
+        if (valor1 < 9 && valor1 < 14) {
             document.getElementById("numero2").innerHTML = "Continue tentando!!";
 
         } var x = 0;
-        document.getElementById("contar").innerHTML = (valor1 != valor2) ? "" : "" ;
+        document.getElementById("contar").innerHTML = (valor1 != valor2) ? "" : "";
     }
 }
 var contarVal = 0;
-function contarClick(){
+function contarClick() {
     updateDisplay(++contarVal);
 }
 
-function resetarContador(){
+function resetarContador() {
     contarVal = 0;
     updateDisplay(contarVal);
 
 }
 
-function updateDisplay(val){
+function updateDisplay(val) {
     document.getElementById("contartentativas").innerHTML = val;
 }
 var btn = document.querySelector("#resetar");
-btn.addEventListener("click", function(){
+btn.addEventListener("click", function () {
     location.reload();
 })
 
-function verificarnumero(){
-    let testel = document.getElementById("testenumero").value;
-    switch(teste1){
+function verificarnumero() {
+    let teste1 = document.getElementById("testenumero").value;
+    switch (teste1) {
         case "2":
             document.getElementById("botao10").style.backgroundColor = "yellow";
             document.getElementById("numero3").innerHTML = "Parabéns, você acertou um número !";
@@ -52,9 +52,9 @@ function verificarnumero(){
 
 var ganhou = math.floor(math.random() * 9) + 1;
 
-function testar(elem){
+function testar(elem) {
     var num = parseInt(elem.innerHTML);
-    switch (num){
+    switch (num) {
         case ganhou:
             document.querySelector("#resposta").innerHTML = "Parabéns!! Você achou a caixa correta!";
             document.querySelector("#resposta").style.backgroundColor = "yellow";
@@ -67,15 +67,15 @@ function testar(elem){
             break;
     }
 }
-function contarClick(){
+function contarClick() {
     updateDisplay2(++contarVal);
 }
-function updateDisplay2(val){
+function updateDisplay2(val) {
     document.getElementById("contartentativas2").innerHTML = val;
 
 }
 
 var btn = document.querySelector("#resetar2");
-btn.addEventListener("click",function(){
+btn.addEventListener("click", function () {
     location.reload();
 })
